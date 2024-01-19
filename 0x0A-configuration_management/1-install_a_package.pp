ebb860c77db55ce9fe09d0e9b8a001c10402ed4a
Cha1mae <chaimastaifa@gmail.com>
2024-01-19 09:24:19 -0800

    tryingg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,7 @@
 # okay so i will try to install flask from pip3
-# so i added python so that Puppet knows to install python3-pip
-Install Python 3
-package { 'python3':
-    ensure   => installed,
-}
-Install Flask using pip3
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Package['python3']
-}
-Install Werkzeug using pip3
-package { 'Werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3',
-    require  => Package['flask']
+package
+{ 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }

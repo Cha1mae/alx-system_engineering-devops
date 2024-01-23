6556793cbd292a8553ba700a7e0b26fbfa1c7aa2
Cha1mae <chaimastaifa@gmail.com>
2024-01-23 10:54:42 -0800

    explained better

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,27 @@
 # this will install nginx web server !!!
 
+# This exec resource updates the system's package list
 exec { 'update system':
   command => '/usr/bin/apt-get update',
 }
+
+# This package resource ensures that Nginx is installed
+# it actually require 'update system' exec resource
 
 package { 'nginx':
   ensure  => 'installed',
   require => Exec['update system'],
 }
 
+# This file resource manages the file at /var/www/html/index.html
+# this will set the content to the specific html string
+
 file { '/var/www/html/index.html':
   content => '<html><body>Hello World!</body></html>',
 }
+
+# This file resource manages the Nginx configuration file at /etc/nginx/sites-available/default
+# set the content to the specific config if it changes ppup will refresh cause of the notify attribute
 
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
@@ -32,6 +42,9 @@ file { '/etc/nginx/sites-available/default':
   ",
   notify  => Service['nginx'],
 }
+
+
+# This service resource manages the Nginx service If the Nginx configuration file changes, Puppet will refresh the service
 
 service { 'nginx':
   ensure    => running,

4be787a7e55a3305b396dc705824735c7f12fb89
Cha1mae <chaimastaifa@gmail.com>
2024-02-19 06:21:28 -0800

    wowie

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Gas will help us gther data from an API :)"""
+"""This will help us gather data from
+an API and export to CSV"""
 
+import csv
 import requests
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: ./0-gather_data_from_an_API.py <employee ID>")
+        print("Usage: ./1-export_to_CSV.py <employee ID>")
         sys.exit(1)
 
     user_id = int(sys.argv[1])
@@ -27,11 +29,8 @@ if __name__ == "__main__":
 
     todos = todos_response.json()
 
-    done_tasks = [task for task in todos if task.get('completed')]
-    total_tasks = len(todos)
-
-    print(f"Employee {user.get('name')} is done with tasks"
-          f"({len(done_tasks)}/{total_tasks}):")
-
-    for task in done_tasks:
-        print("\t", task.get('title'))
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            taskwriter.writerow([user_id, user.get('name'),
+                                 task.get('completed'), task.get('title')])

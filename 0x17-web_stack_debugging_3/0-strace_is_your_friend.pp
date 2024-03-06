33e1ba3c1d40fdc813383977289f652d15c314c5
Cha1mae <chaimastaifa@gmail.com>
2024-03-06 05:11:12 -0800

    just checking

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,9 @@
 # How to fix the Apache 500 error ez
-file { '0-strace_is_your_friend.pp':
-  ensure  => file,
-  path    => '/root/0-strace_is_your_friend.pp',
-  content => '
-    # Use the exec resource type to run shell commands
-    exec { "fix-apache":
-      command => "/usr/bin/apt-get install -y apache2 && service apache2 restart",
-      path    => ["/usr/bin", "/usr/sbin"],
-    }
-  ',
+# This code executes a shell command using sed to
+# replace all instances of "phpp" with "php" in the
+# file /var/www/html/wp-settings.php
+exec 
+{ 'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
